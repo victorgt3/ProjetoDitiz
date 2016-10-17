24db0803461e920b2cbabce14dee561bbad78337
Anderson <guita.victor@gmail.com>
2016-10-16 23:38:33 -0200

    Layout

--- resources/views/produto/index.blade.php
+++ resources/views/produto/index.blade.php
@@ -43,20 +43,55 @@
                         
                     </table>
 
-                    <div align="center">
-                        {!! $produtos->links() !!}
-                    </div>
+                  
 
             </div>
             <!-- /.box-body -->
-          </div>
+          
           <!-- /.box -->
 
           
           <!-- /.box -->
         </div>
         <!-- /.col -->
-      </div>
+      
+       <div class="box">
+
+            <div class="box-header">
+              <h3 class="box-title">Data Table With Full Features</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+            <p>
+                        <a class="btn btn-info" href="{{ route('produto.adicionar') }}">Adicinar</a>
+                    </p>
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                                <th>Codigo</th>
+                                <th>Descrição</th>
+                                <th>Preço</th>
+                                <th>Ação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($produtos as $produto)
+
+                            <tr>
+                                <th scope="row">{{ $produto->id }}</th>
+                                <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->preco }}</td>
+                                <td>
+                                    <a class="btn btn-default" href="{{route('produto.editar',$produto->id)}}">Editar</a>
+                                    <a class="btn btn-danger" href="javascript:(confirm('Deletar esse registro?') ? window.location.href='{{route('produto.deletar',$produto->id)}}' : false)">Deletar</a>
+                                </td>
+                            </tr>                            
+
+                            @endforeach
+                            
+                        </tbody>
+              </table>
+            </div>
       <!-- /.row -->
     </section>
     @endsection
